d3c01c405ea2d1e63b6b8afedb6a2d929256f62c
maartensijmkens <maarten.sijmkens@gmail.com>
2020-12-04 18:08:31 +0100

    day4 remove redundancy

--- 2020/day4/main.js
+++ 2020/day4/main.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { PassThrough } = require('stream');
 
 function getValues() {
     let values = fs.readFileSync('input.txt', "utf8").split("\n\n");
@@ -16,22 +15,21 @@ function part1(values) {
     return values.filter(password => required.every(r => password.hasOwnProperty(r))).length
 }
 
-function testQuantity(string, lb = -Infinity, ub = Infinity, unit = '') {
+function testQuantity(string, l, lb = -Infinity, ub = Infinity, unit = '') {
     n = parseInt(string);
-    return string.endsWith(unit) && !isNaN(n) && n >= lb && n <= ub;
+    return (!l || string.length == l) && string.endsWith(unit) && n >= lb && n <= ub;
 }
 
 function part2(values) {
-    let total = 0;
     const required = ['byr','iyr','eyr','hgt','hcl','ecl','pid'];
     const tests = [
-        (vr) => vr.length == 4 && testQuantity(vr, 1920, 2002),
-        (vr) => vr.length == 4 && testQuantity(vr, 2010, 2020),
-        (vr) => vr.length == 4 && testQuantity(vr, 2020, 2030),
-        (vr) => testQuantity(vr, 150, 193, 'cm') || testQuantity(vr, 59, 76, 'in'),
+        (vr) => testQuantity(vr, 4, 1920, 2002),
+        (vr) => testQuantity(vr, 4, 2010, 2020),
+        (vr) => testQuantity(vr, 4, 2020, 2030),
+        (vr) => testQuantity(vr, null, 150, 193, 'cm') || testQuantity(vr, null, 59, 76, 'in'),
         (vr) => new RegExp(/^#([a-fA-F0-9]{6})$/).test(vr),
         (vr) => ['amb','blu','brn','gry','grn','hzl','oth'].includes(vr),
-        (vr) => vr.length == 9 && testQuantity(vr) && !isNaN(vr)
+        (vr) => testQuantity(vr, 9) && !isNaN(vr)
     ];
     return values.filter(password => required.every((r,i) => password[r] && tests[i](password[r]))).length
 }
