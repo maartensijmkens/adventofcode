31b7354342b4ed0a9a2659e23051619c515ed690
maartensijmkens <maarten.sijmkens@gmail.com>
2020-12-04 17:59:06 +0100

    compact day4

--- 2020/day4/main.js
+++ 2020/day4/main.js
@@ -7,61 +7,33 @@ function getValues() {
 }
 
 function decodeLine(line) {
-    const data = {}
-    pairs = line.split(/ |\n/);
-    for (p of pairs) {
-        [field, value] = p.split(":");
-        data[field] = value
-    }
-    return data;
+    pairs = line.split(/ |\n/).map(p => p.split(':'));
+    return Object.fromEntries(pairs);
 }
 
 function part1(values) {
-    let total = 0;
     const required = ['byr','iyr','eyr','hgt','hcl','ecl','pid'];
-    for (v of values) {
-        let valid = true;
-        for (r of required) {
-            if (!v[r]) valid = false;
-        }
-        if (valid) total++;
-    }
-    return total;
+    return values.filter(password => required.every(r => password.hasOwnProperty(r))).length
+}
+
+function testQuantity(string, lb = -Infinity, ub = Infinity, unit = '') {
+    n = parseInt(string);
+    return string.endsWith(unit) && !isNaN(n) && n >= lb && n <= ub;
 }
 
 function part2(values) {
     let total = 0;
     const required = ['byr','iyr','eyr','hgt','hcl','ecl','pid'];
     const tests = [
-        (vr) => vr.length == 4 && !isNaN(vr) && parseInt(vr) >= 1920 && parseInt(vr) <= 2002,
-        (vr) => vr.length == 4 && !isNaN(vr) && parseInt(vr) >= 2010 && parseInt(vr) <= 2020,
-        (vr) => vr.length == 4 && !isNaN(vr) && parseInt(vr) >= 2020 && parseInt(vr) <= 2030,
-        (vr) => {
-            cm = parseInt(vr.replace('cm', ''));
-            inch = parseInt(vr.replace('in', ''));
-            return vr.includes('cm') && cm >= 150 && cm <= 193 || 
-            vr.includes('in') && inch >= 59 && inch <= 76;
-        },
+        (vr) => vr.length == 4 && testQuantity(vr, 1920, 2002),
+        (vr) => vr.length == 4 && testQuantity(vr, 2010, 2020),
+        (vr) => vr.length == 4 && testQuantity(vr, 2020, 2030),
+        (vr) => testQuantity(vr, 150, 193, 'cm') || testQuantity(vr, 59, 76, 'in'),
         (vr) => new RegExp(/^#([a-fA-F0-9]{6})$/).test(vr),
-        (vr) => {
-            colors = ['amb','blu','brn','gry','grn','hzl','oth'];
-            return colors.includes(vr);
-        },
-        (vr) => vr.length == 9 && !isNaN(vr)
+        (vr) => ['amb','blu','brn','gry','grn','hzl','oth'].includes(vr),
+        (vr) => vr.length == 9 && testQuantity(vr) && !isNaN(vr)
     ];
-    for (v of values) {
-        let valid = true;
-        for (i in required) {
-            r = required[i];
-            t = tests[i];
-            if(!v[r] || !t(v[r])) {
-                valid = false;
-                break;
-            }
-        }
-        if (valid) total++;
-    }
-    return total;
+    return values.filter(password => required.every((r,i) => password[r] && tests[i](password[r]))).length
 }
 
 function main() {
